5da46f98aa4d406f3c06c664c5317ca89c1c956f
Pzon <piotrzon@gmail.com>
2020-12-11 15:19:33 +0100

    few methods in MainWindowController done

--- src/app/controller/MainWindowController.java
+++ src/app/controller/MainWindowController.java
@@ -4,14 +4,26 @@
  **/
 package app.controller;
 
+import app.model.WeatherManager;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
-public class MainWindowController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindowController implements Initializable {
+    WeatherManager weatherManager;
+    String citySet;
+
+    public MainWindowController() {
+        this.citySet = "Krakow".toUpperCase();
+    }
 
     @FXML
     private TextField cityNameInput;
@@ -20,16 +32,16 @@ public class MainWindowController {
     private Button findCityButton;
 
     @FXML
-    private Button CancelActionButton;
+    private Button setButton;
 
     @FXML
-    private Button CancelActionButton1;
+    private Button cancelButton;
 
     @FXML
     private Pane weatherInfoPane;
 
     @FXML
-    private Label TownName;
+    private Label city;
 
     @FXML
     private Label day;
@@ -62,9 +74,52 @@ public class MainWindowController {
     private Label humidity;
 
     @FXML
-    void ButtonOnAction() {
+    void ButtonOnAction(javafx.event.ActionEvent actionEvent) {
+        String initialCity = city.getText();
 
+        if(actionEvent.getSource() == findCityButton){
+            cityNameInput.setText("");
+            bottomSet(true);
+            cityNameInput.requestFocus();
+        }else if(actionEvent.getSource() == setButton){
+            setPressed();
+        }else if(actionEvent.getSource() == cancelButton){
+            cityNameInput.setText(initialCity);
+            bottomSet(false);
+            invis.requestFocus();
+        }
     }
 
+    private void setPressed() {
+        if(cityNameInput.getText().equals("")){
+            showError("City name can not be empty");
+            return;
+        }else{
+
+        }
+    }
+
+    private void showError(String txt) {
+        errors.setText(txt);
+        errors.setTextFill(Color.RED);
+    }
+
+    private void bottomSet(boolean b) {
+        cityNameInput.setDisable(!b);
+        setButton.setVisible(b);
+        findCityButton.setVisible(!b);
+        cancelButton.setVisible(b);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        cityNameInput.setText(citySet);
+        cityNameInput.setDisable(true);
+        setButton.setVisible(false);
+        cancelButton.setVisible(false);
+        errors.setText("");
+        invis.requestFocus();
+        weatherManager = new WeatherManager(citySet);
+    }
 }
 

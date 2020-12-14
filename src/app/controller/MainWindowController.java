3ddaf931ecf0bd45a5acb062c4cc611003da96c0
Pzon <piotrzon@gmail.com>
2020-12-14 15:13:06 +0100

    MainController class almost done - displayWeather method to do

--- src/app/controller/MainWindowController.java
+++ src/app/controller/MainWindowController.java
@@ -5,17 +5,22 @@
 package app.controller;
 
 import app.model.WeatherManager;
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
 public class MainWindowController implements Initializable {
     WeatherManager weatherManager;
@@ -74,7 +79,7 @@ public class MainWindowController implements Initializable {
     private Label humidity;
 
     @FXML
-    void ButtonOnAction(javafx.event.ActionEvent actionEvent) {
+    void buttonOnAction(javafx.event.ActionEvent actionEvent) {
         String initialCity = city.getText();
 
         if(actionEvent.getSource() == findCityButton){
@@ -95,13 +100,60 @@ public class MainWindowController implements Initializable {
             showError("City name can not be empty");
             return;
         }else{
-
+            try{
+                errors.setText("");
+                this.citySet = cityNameInput.getText().trim();
+                cityNameInput.setText(this.citySet.toUpperCase());
+                weatherManager = new WeatherManager(this.citySet);
+                displayWeather();
+                bottomSet(false);
+                invis.requestFocus();
+            }catch (Exception e){
+                city.setText("Error");
+                city.setTextFill(Color.TOMATO);
+                showError("No location found, please try again");
+                reset();
+                invis.requestFocus();
+            }
         }
     }
 
-    private void showError(String txt) {
-        errors.setText(txt);
-        errors.setTextFill(Color.RED);
+    private void reset() {
+        bottomSet(false);
+        day.setText("");
+        desc.setText("");
+        img.setImage(null);
+        temperature.setText("");
+        windSpeed.setText("");
+        cloudiness.setText("");
+        pressure.setText("");
+        humidity.setText("");
+    }
+
+    private void displayWeather() {
+    }
+
+    private void showError(String txt)  {
+        city.setText(txt);
+        city.setTextFill(Color.RED);
+        city.setStyle("-fx-border-color: red; -fx-border-radius: 13; -fx-border-width: 2; -fx-padding: 2;");
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), city);
+        fadeIn.setToValue(1);
+        fadeIn.setFromValue(0);
+        fadeIn.play();
+
+        fadeIn.setOnFinished(event -> {
+            PauseTransition pause = new PauseTransition(Duration.seconds(2));
+            pause.play();
+            pause.setOnFinished(event2 -> {
+                FadeTransition fadeOut = new FadeTransition(Duration.seconds(2), city);
+                fadeOut.setToValue(0);
+                fadeOut.setFromValue(1);
+                fadeOut.play();
+            });
+        });
+        cityNameInput.setText("");
+
     }
 
     private void bottomSet(boolean b) {
@@ -120,6 +172,23 @@ public class MainWindowController implements Initializable {
         errors.setText("");
         invis.requestFocus();
         weatherManager = new WeatherManager(citySet);
+
+        try{
+            displayWeather();
+        }catch (Exception e){
+            city.setText("Error - internet connection");
+            city.setTextFill(Color.DARKSALMON);
+            showError("No internet connection");
+            reset();
+            findCityButton.setDisable(true);
+            cityNameInput.setText("");
+        }
+
+        cityNameInput.setOnKeyPressed(e->{
+            if(e.getCode() == KeyCode.ENTER){
+                setPressed();
+            }
+        });
     }
 }
 
